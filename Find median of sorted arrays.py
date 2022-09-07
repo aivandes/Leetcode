965d0bbac1163b65c755b779571194273b5228e8
Iván González Schulten <iagonzalez7@uc.cl>
2022-09-07 02:39:02 -0300

    Update Find median of sorted arrays.py

--- Find median of sorted arrays.py
+++ Find median of sorted arrays.py
@@ -1,3 +1,4 @@
+#------------------------O(n**2) and O(1) time-space----------------------------------------#
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         a = []
@@ -13,3 +14,26 @@ class Solution:
         a += nums1 + nums2
         print(a)
         return a[half // 2] if half % 2 != 0 else (a[half //2] + a[(half - 1) // 2]) / 2
+    #-----------------------------O(n) time-space -------------------------------------------#
+    
+    class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        a = []
+        half = (len(nums1) + len(nums2))
+        idx = 0
+        idy = 0
+        
+        while True:
+            
+            if idx == len(nums1):
+                a += nums2[idy:]
+                break
+            elif idy == len(nums2):
+                a += nums1[idx:]
+                break
+            if nums1[idx] < nums2[idy]:
+                a.append(nums1[idx])
+                idx += 1
+            else:
+                a.append(nums2[idy])
+                idy += 1
