b93a5432c6e2ca1e99a3cb7f1f119cdd659a9980
Iván González Schulten <iagonzalez7@uc.cl>
2022-09-08 04:20:17 -0300

    Create Search insert Position.py

---
+++ Search insert Position.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        
+        while l < r:
+            mid = l + (r - l) // 2
+            
+            if target == nums[mid]:
+                return mid
+            
+            elif target < nums[mid]:
+                r = mid
+            
+            elif target > nums[mid]:
+                l = mid + 1
+            
+            
+        return l + 1 if nums[l] < target else l
